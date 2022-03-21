b33caba3f5ff63980126fb404b6fc76b5a77986c
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2022-03-21 17:07:58 +0700

    install casual module from npm

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const testModule = require('./testModule');
+const casual = require('casual');
 
 const hostname = '127.0.0.1';
 const port = 2000;
@@ -8,7 +9,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 //   res.end('Hello, World!\n');
-  res.end(testModule);
+  res.end(casual.sentence);
 });
 
 server.listen(port, hostname, () => {
